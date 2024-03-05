61f4b661a2c813c31d500cc664a6efb9cfdc0011
vincent-doan <vinh.dt210940@sis.hust.edu.vn>
2024-03-06 00:29:40 +0700

    fix(calculation) fix circle area func

--- calculation.py
+++ calculation.py
@@ -4,7 +4,7 @@ def cal_rectangle_perimeter(a, b):
     return 2*(a + b)
 
 def cal_circle_area(r):
-    return 2*math.pi * pow(r, 2)
+    return math.pi * pow(r, 2)
 
 if __name__ == '__main__':
     f = int(input("choose function: \n\
